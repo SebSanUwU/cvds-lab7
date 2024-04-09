b3e3713fe20bb5f1103078fdeb76a9908efdd757
Redes <diego.fercho.castellanos@gmail.com>
2024-04-09 18:05:55 -0500

    c

--- src/main/java/co/edu/escuelaing/cvds/lab7/Lab7Application.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/Lab7Application.java
@@ -1,9 +1,7 @@
 package co.edu.escuelaing.cvds.lab7;
 
 import co.edu.escuelaing.cvds.lab7.model.Configuration;
-import co.edu.escuelaing.cvds.lab7.model.Employee;
 import co.edu.escuelaing.cvds.lab7.service.ConfigurationService;
-import co.edu.escuelaing.cvds.lab7.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,21 +10,20 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Lab7Application {
-	@Autowired
-	ConfigurationService configurationService;
-	@Autowired
-	EmployeeService employeeService;
 
+	ConfigurationService configurationService;
+
+	@Autowired
 	public static void main(String[] args) {
 		SpringApplication.run(Lab7Application.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner run() throws Exception {
+	public CommandLineRunner run(){
 		return (args) -> {
 
 			System.out.println("Adding Configurations....");
-			configurationService.addConfiguration(new Configuration("premio", "800000"));
+			configurationService.addConfiguration(new Configuration("price", "800000"));
 
 			System.out.println("\nGetting all configurations....");
 			configurationService.getAllConfigurations().forEach(configuration -> System.out.println(configuration));
