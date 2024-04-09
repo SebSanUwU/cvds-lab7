3a971740831b616635773d1977b42a5686a52cb9
SebSanUwU <juancamargo12@hotmail.com>
2024-04-09 18:55:59 -0500

    More fixes

--- src/main/java/co/edu/escuelaing/cvds/lab7/controller/EmployeeController.java
+++ src/main/java/co/edu/escuelaing/cvds/lab7/controller/EmployeeController.java
@@ -3,20 +3,20 @@ package co.edu.escuelaing.cvds.lab7.controller;
 
 import co.edu.escuelaing.cvds.lab7.model.Employee;
 import co.edu.escuelaing.cvds.lab7.service.EmployeeService;
-import jakarta.persistence.Access;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-
+    private final EmployeeService employeeService;
     @Autowired
-    EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @GetMapping("/list")
     public String employees(Model model) {
         model.addAttribute("empleados",employeeService.getAllEmployees());
